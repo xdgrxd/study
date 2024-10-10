7c414660549b2c34d6e7f2c0d7976ca42fb4e0a9
Guilherme Almeida <xdgrxd.dev@gmail.com>
2024-10-10 07:45:10 -0300

    first req http local server

--- 1-start-react/7-req-http/httpreact/src/App.jsx
+++ 1-start-react/7-req-http/httpreact/src/App.jsx
@@ -1,14 +1,77 @@
-import { useState } from 'react'
-import './App.css'
+import "./App.css";
+import { useState, useEffect } from "react";
+
+const url = "http://localhost:3000/products";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [products, setProducts] = useState([]);
+  const [name, setName] = useState("");
+  const [price, setPrice] = useState("");
+
+  useEffect(() => {
+    async function fetchData() {
+      const res = await fetch(url);
+      const data = await res.json();
+
+      setProducts(data);
+
+      return data;
+    }
+    fetchData();
+  }, []);
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    const product = {
+      name,
+      price,
+    };
+
+    const res = await fetch(url, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+
+      body: JSON.stringify(product),
+    });
+  };
 
   return (
     <>
       <h1>Product List</h1>
+      <ul>
+        {products.map((item, i) => (
+          <li key={i}>
+            {item.name} - U${item.price}
+          </li>
+        ))}
+      </ul>
+
+      <div className="addProduct">
+        <form onSubmit={handleSubmit}>
+          <label>
+            Name:
+            <input
+              type="text"
+              value={name}
+              name="name"
+              onChange={(e) => setName(e.target.value)}
+            />
+            <input
+              type="number"
+              value={price}
+              name="price"
+              onChange={(e) => setPrice(e.target.value)}
+            />
+          </label>
+
+          <input type="submit" value="Add now" />
+        </form>
+      </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
