7e88dac2c39495fee97b45de5bdf1a50a01df9fe
Guilherme Almeida <xdgrxd.dev@gmail.com>
2024-11-13 20:20:19 -0300

    post page estilization

--- 1-start-react/10-mini-blog/src/pages/Home/Home.jsx
+++ 1-start-react/10-mini-blog/src/pages/Home/Home.jsx
@@ -1,7 +1,40 @@
 import styles from "./Home.module.css";
 
+import { useNavigate, Link } from "react-router-dom";
+import { useState } from "react";
+
 const Home = () => {
-  return <div></div>;
+  const [query, setQuery] = useState("");
+  const [posts] = useState([]);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+  };
+
+  return (
+    <div className={styles.home}>
+      <h1>See the latest posts!</h1>
+      <form onSubmit={handleSubmit} className={styles.searchForm}>
+        <input
+          type="text"
+          placeholder="Search anything here"
+          onChange={(e) => setQuery(e.target.value)}
+        />
+        <button className="btn dark">Search</button>
+      </form>
+      <div>
+        <h1>Posts</h1>
+        {posts && posts.length === 0 && (
+          <div className={styles.noposts}>
+            <p>Not found posts</p>
+            <Link to="/posts/create" className="btn">
+              Create first post
+            </Link>
+          </div>
+        )}
+      </div>
+    </div>
+  );
 };
 
 export default Home;
