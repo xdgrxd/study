1fa1439f41803044fdbd4948f05726bb704d2983
Guilherme Almeida <xdgrxd.dev@gmail.com>
2024-11-21 23:55:03 -0300

    search render

--- 1-start-react/10-mini-blog/src/pages/Search/Search.jsx
+++ 1-start-react/10-mini-blog/src/pages/Search/Search.jsx
@@ -3,14 +3,28 @@ import styles from "./Search.module.css";
 import { useFetchDocuments } from "../../hooks/useFetchDocuments";
 import { useQuery } from "../../hooks/useQuery";
 
+import PostDetail from "../../components/PostDetail";
+import { Link } from "react-router-dom";
+
 const Search = () => {
   const query = useQuery();
   const search = query.get("q");
 
+  const { documents: posts } = useFetchDocuments("posts", search);
+
   return (
-    <div>
+    <div className={styles.search_container}>
       <h2>Search</h2>
-      {search && search}
+
+      {posts && posts.length === 0 && (
+        <div className={styles.noposts}>
+          <p>Not posts found.</p>
+          <Link to="/" className="btn-dat">
+            Back
+          </Link>
+        </div>
+      )}
+      {posts && posts.map((post) => <PostDetail key={post.id} post={post} />)}
     </div>
   );
 };
