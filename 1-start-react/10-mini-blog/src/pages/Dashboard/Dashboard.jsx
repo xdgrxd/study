16c41ec7d123b11453f3f678016d2e3fef5eb7cf
Guilherme Almeida <xdgrxd.dev@gmail.com>
2024-11-22 21:32:21 -0300

    dashboard structure

--- 1-start-react/10-mini-blog/src/pages/Dashboard/Dashboard.jsx
+++ 1-start-react/10-mini-blog/src/pages/Dashboard/Dashboard.jsx
@@ -1,9 +1,32 @@
 import styles from "./Dashboard.module.css";
 
+import { Link } from "react-router-dom";
+
+import { useAuthValue } from "../../contexts/AuthContext";
+import { useFetchDocument } from "../../hooks/useFetchDocument";
+
 const Dashboard = () => {
-  return <div>
-    <h1>Dashboard</h1>
-  </div>;
+  const { user } = useAuthValue();
+  const uid = user.uid;
+
+  const posts = [];
+
+  return (
+    <div>
+      <h1>Dashboard</h1>
+      <p>Manage your posts!</p>
+      {posts && posts.length === 0 ? (
+        <div>
+          <p>Posts not found.</p>
+          <Link to="/posts/create/" className="btn">Create first post</Link>
+        </div>
+      ) : (
+        <div>
+          <p>Posts!</p>
+        </div>
+      )}
+    </div>
+  );
 };
 
 export default Dashboard;
