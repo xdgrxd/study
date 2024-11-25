fb6c327ac4e163f359ddadfb9c093bd19790be74
Guilherme Almeida <xdgrxd.dev@gmail.com>
2024-11-25 19:00:59 -0300

    dashboard starts

--- 1-start-react/10-mini-blog/src/pages/Dashboard/Dashboard.jsx
+++ 1-start-react/10-mini-blog/src/pages/Dashboard/Dashboard.jsx
@@ -3,13 +3,17 @@ import styles from "./Dashboard.module.css";
 import { Link } from "react-router-dom";
 
 import { useAuthValue } from "../../contexts/AuthContext";
-import { useFetchDocument } from "../../hooks/useFetchDocument";
+import { useFetchDocuments } from "../../hooks/useFetchDocuments";
 
 const Dashboard = () => {
   const { user } = useAuthValue();
   const uid = user.uid;
 
-  const posts = [];
+  const {
+    documents: posts,
+    loading,
+    error,
+  } = useFetchDocuments("posts", null, uid);
 
   return (
     <div>
@@ -18,13 +22,17 @@ const Dashboard = () => {
       {posts && posts.length === 0 ? (
         <div>
           <p>Posts not found.</p>
-          <Link to="/posts/create/" className="btn">Create first post</Link>
+          <Link to="/posts/create/" className="btn">
+            Create first post
+          </Link>
         </div>
       ) : (
         <div>
           <p>Posts!</p>
         </div>
       )}
+
+      {posts && posts.map((post) => <h3 key={post.id}>{post.title}</h3>)}
     </div>
   );
 };
