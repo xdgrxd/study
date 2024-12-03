433460dcc972e4a7013a39a0a74cf81cac9d9c54
Guilherme Almeida <xdgrxd.dev@gmail.com>
2024-12-02 21:45:06 -0300

    input useState

--- 1-start-react/11-hooks/src/components/HookUseState.jsx
+++ 1-start-react/11-hooks/src/components/HookUseState.jsx
@@ -9,6 +9,14 @@ const HookUseState = () => {
     setName("New Joseph");
   };
 
+  const [age, setAge] = useState(18);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    console.log(age);
+  };
+
   return (
     <div>
       <h2>useState</h2>
@@ -19,6 +27,16 @@ const HookUseState = () => {
       <button onClick={changeNames}>Change names</button>
 
       <hr />
+
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          value={age}
+          onChange={(e) => setAge(e.target.value)}
+        />
+        <input type="submit" value="Test" />
+      </form>
+      <p>You are {age} years old!</p>
     </div>
   );
 };
