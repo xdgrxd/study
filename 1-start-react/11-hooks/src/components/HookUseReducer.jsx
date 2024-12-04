ccac9cb183b563fe228c834ea6da6b69a8d2281a
Guilherme Almeida <xdgrxd.dev@gmail.com>
2024-12-03 22:29:52 -0300

    useReducer and actions

--- 1-start-react/11-hooks/src/components/HookUseReducer.jsx
+++ 1-start-react/11-hooks/src/components/HookUseReducer.jsx
@@ -1,15 +1,67 @@
-import { useReducer } from "react";
+import { useReducer, useState } from "react";
 
 const HookUseReducer = () => {
   const [number, dispatch] = useReducer((state, action) => {
     return Math.random(state);
   });
 
-  return <div>
-    <h2>useReducer</h2>
-    <p>Number: {number}</p>
-    <button onClick={dispatch}>Change number</button>
-  </div>;
+  const initialTasks = [
+    { id: 1, text: "Make A" },
+    { id: 2, text: "Make B" },
+  ];
+
+  const taskReducer = (state, action) => {
+    switch (action.type) {
+      case "ADD":
+        const newTask = {
+          id: Math.random(),
+          text: taskText,
+        };
+
+        setTaskText("");
+
+        return [...state, newTask];
+      case "DELETE":
+        return state.filter((task) => task.id !== action.id);
+      default:
+        return state;
+    }
+  };
+
+  const [taskText, setTaskText] = useState("");
+  const [tasks, dispatchTasks] = useReducer(taskReducer, initialTasks);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    dispatchTasks({ type: "ADD" });
+  };
+
+  const removeTask = (id) => {
+    dispatchTasks({ type: "DELETE", id });
+  };
+
+  return (
+    <div>
+      <h2>useReducer</h2>
+      <p>Number: {number}</p>
+      <button onClick={dispatch}>Change number</button>
+      <h3>Tasks</h3>
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          onChange={(e) => setTaskText(e.target.value)}
+          value={taskText}
+        />
+        <input type="submit" value="Add" />
+      </form>
+      {tasks.map((task) => (
+        <li key={task.id} onDoubleClick={() => removeTask(task.id)}>
+          {task.text}
+        </li>
+      ))}
+    </div>
+  );
 };
 
 export default HookUseReducer;
