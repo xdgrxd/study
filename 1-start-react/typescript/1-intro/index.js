c8af6eb7a21e4410b7813bff828301a1590685fc
Guilherme Almeida <xdgrxd.dev@gmail.com>
2024-12-09 21:28:34 -0300

    start typescript

---
+++ 1-start-react/typescript/1-intro/index.js
@@ -0,0 +1,9 @@
+var firstName = "John";
+var anotherName = 1;
+var x = true;
+function greeting(name) {
+    console.log("Hello, " + name);
+}
+greeting(firstName);
+greeting(anotherName);
+greeting(x);
---
+++ 1-start-react/typescript/1-intro/index.ts
@@ -0,0 +1,11 @@
+const firstName = "John";
+const anotherName = 1;
+const x = true;
+
+function greeting(name: string) {
+  console.log("Hello, " + name);
+}
+
+greeting(firstName);
+greeting(anotherName);
+greeting(x);
