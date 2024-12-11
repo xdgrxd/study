a71deb408c848428a8f327eccfaceeda0f5db076
Guilherme Almeida <xdgrxd.dev@gmail.com>
2024-12-11 00:04:19 -0300

    mui install

--- 2-react-ts-mui/src/App.tsx
+++ 2-react-ts-mui/src/App.tsx
@@ -1,8 +1,10 @@
-export const App = () => {
+import { BrowserRouter } from "react-router-dom";
+import { AppRoutes } from "./routes";
 
+export const App = () => {
   return (
-    <>
-      
-    </>
-  )
-}
+    <BrowserRouter>
+        <AppRoutes />
+    </BrowserRouter>
+  );
+};
---
+++ 2-react-ts-mui/src/routes/index.tsx
@@ -0,0 +1,11 @@
+import { Button } from "@mui/material";
+import { Routes, Route, Navigate } from "react-router-dom";
+
+export const AppRoutes = () => {
+  return (
+    <Routes>
+      <Route path="/home" element={<Button>Test</Button>} />
+      <Route path="*" element={<Navigate to="/home"/>} />
+    </Routes>
+  );
+};
