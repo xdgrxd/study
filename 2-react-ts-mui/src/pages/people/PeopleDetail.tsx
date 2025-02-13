793079a8684e8dad836363171001f0086ea276e4
gui <xdgrxd.dev@gmail.com>
2025-02-13 19:43:05 -0300

    feat: people detail upgrades

--- 2-react-ts-mui/src/pages/people/PeopleDetail.tsx
+++ 2-react-ts-mui/src/pages/people/PeopleDetail.tsx
@@ -1,23 +1,56 @@
 import { useNavigate, useParams } from 'react-router-dom';
 import { DefaultPageLayout } from '../../shared/layouts';
 import { DetailTools } from '../../shared/components';
-import { Typography } from '@mui/material';
+import { Input, LinearProgress, Typography } from '@mui/material';
+import { useEffect, useState } from 'react';
+import { PeopleService } from '../../shared/services/api/people/PeopleService';
 
 export const PeopleDetail: React.FC = () => {
   const { id = 'new' } = useParams<'id'>();
   const navigate = useNavigate();
 
+  const [isLoading, setIsLoading] = useState(false);
+  const [titleUserName, setTitleUserName] = useState('');
+
+  useEffect(() => {
+    if (id !== 'new') {
+      setIsLoading(true);
+
+      PeopleService.getById(Number(id)).then((result) => {
+        setIsLoading(false);
+        if (result instanceof Error) {
+          alert(result.message);
+          navigate('/people');
+        } else {
+          setTitleUserName(result.fullName);
+          console.log(result);
+        }
+      });
+    }
+  }, [id]);
+
   const handleSave = () => {
     console.log('Save');
   };
 
-  const handleDelete = () => {
-    console.log('Delete');
+  const handleDelete = (id: number) => {
+    if (confirm('Do you want to delete this?')) {
+      PeopleService.deleteById(id).then((result) => {
+        if (result instanceof Error) {
+          alert(result.message);
+        } else {
+          alert('Register has been deleted!');
+          navigate('/people');
+        }
+      });
+    }
   };
+
+  const [typeUserName, setTypeUserName] = useState('');
 
   return (
     <DefaultPageLayout
-      title="Person Detail"
+      title={id === 'new' ? typeUserName || 'New person' : titleUserName}
       toolBar={
         <DetailTools
           buttonNewText="New"
@@ -30,7 +63,7 @@ export const PeopleDetail: React.FC = () => {
             navigate('/people/');
           }}
           onClickDelete={() => {
-            handleDelete;
+            handleDelete(Number(id));
           }}
           onClickSave={() => {
             handleSave;
@@ -38,6 +71,8 @@ export const PeopleDetail: React.FC = () => {
         />
       }
     >
+      {isLoading && <LinearProgress variant="indeterminate" />}
+
       <Typography variant="h1">Content here</Typography>
     </DefaultPageLayout>
   );
